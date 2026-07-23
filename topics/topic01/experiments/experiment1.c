e215c423f292b51e4ada9d3d25b21390bf35081e
thickovt-sys <chickovt@gmail.com>
2026-07-23 04:29:16 +0300

    Reorganize repository structure

---
+++ topics/topic01/experiments/experiment1.c
@@ -0,0 +1,11 @@
+int sum(int a, int b)
+
+int main(void)
+{
+    return sum(2, 3);
+}
+
+int sum(int a, int b)
+{
+    return a + b;
+}
---
+++ topics/topic01/experiments/experiment2.c
@@ -0,0 +1,9 @@
+int main(void)
+{
+    return sum(2, 3);
+}
+
+int sum(int a, int b)
+{
+    return a + b;
+}
